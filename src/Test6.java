2d87392c66140c374d9075ab1a11848c79feda7c
yincx <yinchengxiang@wkbins.com>
2017-04-08 14:00:53 +0800

    测试

--- src/Test6.java
+++ src/Test6.java
@@ -3,10 +3,11 @@ public class Test6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello the world");
+		System.out.println("hello the world!\t");
 		System.out.println("bb");
 		System.out.println("sss");
 		System.out.println("cccc");
+		System.out.println("hhhh");
 	}
 
 }
