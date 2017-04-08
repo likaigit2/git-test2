559c2d8c1707111991028aaa1c8346f737536e3b
likai <likai@wkbins.com>
2017-04-08 14:12:24 +0800

    ceshi

--- src/Test6.java
+++ src/Test6.java
@@ -3,10 +3,9 @@ public class Test6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello the world!\n");
+		System.out.println("hello the world!\r");
 		System.out.println("bb");
 		System.out.println("sss");
-		System.out.println("cccc");
 		System.out.println("dddd");
 	}
 
